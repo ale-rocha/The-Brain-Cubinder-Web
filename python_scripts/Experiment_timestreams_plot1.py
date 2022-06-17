37e34f20d1649b4228d15a2bb8d4b4358e8c6868
ale-rocha <a.rochasolache@gmail.com>
2022-06-16 20:17:28 -0500

    update

--- python_scripts/Experiment_timestreams_plot1.py
+++ python_scripts/Experiment_timestreams_plot1.py
@@ -8,16 +8,28 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+from matplotlib import rcParams as rc
+from matplotlib.pyplot import figure
+from matplotlib import pyplot as plt
 import plotly.express as px
+import os
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import chart_studio
-username = "rochasolache"
-api_key ='abtA3JrpQUvWsIGfaiAk'
 
-chart_studio.tools.set_credentials_file(username=username, api_key=api_key)
-import chart_studio.plotly as py
-import chart_studio.tools as tools
+plt.style.use('classic')
+
+SMALL_SIZE = 10
+MEDIUM_SIZE = 12
+BIGGER_SIZE = 14
+
+plt.rc('font', size=MEDIUM_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=MEDIUM_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=MEDIUM_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 path_experiment ="/Users/alerocha/Documents/Causal-Manifold/Experiments/Experiment1/Outputs/"
 
@@ -30,10 +42,225 @@ theme= "plotly_white"
 channels = np.genfromtxt(path_experiment+"TimeStreams/timestreams.csv", delimiter=',')
 timestamps = np.genfromtxt(path_experiment+"TimeStreams/timestamp.csv", delimiter=',')
 print(timestamps.shape)
-channels = pd.DataFrame(data={'TS':[timestamps[:],timestamps[:],timestamps[:],timestamps[:],timestamps[:],timestamps[:],timestamps[:],timestamps[:]],
-                              'Name': ['Ch1','Ch2','Ch3','Ch4','Ch5','Ch6','Ch7','Ch8'],
-                              'Signal':[channels[:,0],channels[:,1],channels[:,2],channels[:,3],channels[:,4],channels[:,5],channels[:,6],channels[:,7]]})
-# Plot channels
-plotchannels = px.line(channels,x='TS',y='Signal',color="Name")
-py.plot(plotchannels, filename="channels", auto_open=True)
+channels = pd.DataFrame(data={  'Ch1-Oxy':channels[:,0],
+                                'Ch2-Oxy':channels[:,1],
+                                'Ch3-Oxy':channels[:,2],
+                                'Ch4-Oxy':channels[:,3],
+                                'Ch5-Oxy':channels[:,4],
+                                'Ch6-Oxy':channels[:,5],
+                                'Ch1-DeOxy':channels[:,6],
+                                'Ch2-DeOxy':channels[:,7],
+                                'Ch3-DeOxy':channels[:,8],
+                                'Ch4-DeOxy':channels[:,9],
+                                'Ch5-DeOxy':channels[:,10],
+                                'Ch6-DeOxy':channels[:,11],
+                                'Control1-Oxy':channels[:,12],
+                                'Control1-DeOxy':channels[:,13],
+                                'Control2-Oxy':channels[:,14],
+                                'Control2-DeOxy':channels[:,15]})
 
+plt.figure( figsize=(6,6),facecolor='white')
+plt.plot(timestamps,channels['Ch1-Oxy'], color="blue", lw=4, label="OxyHb")
+plt.plot(timestamps,channels['Ch1-DeOxy'],color = "red", lw=4,label="DeoxyHb")
+plt.axhline(y=0,xmin=0,xmax=timestamps.shape[0],color="black", linestyle = '--',alpha=0.4)
+plt.title("Channel 1"); plt.legend(loc="best")
+plt.xlabel("Time [s]"); plt.ylabel(r'$\Delta c$(OxyHb, DeoxyHb)/$\mu$M')
+
+plt.figure( figsize=(6,6),facecolor='white')
+plt.plot(timestamps,channels['Ch2-Oxy'], color="blue", lw=4,label="OxyHb")
+plt.plot(timestamps,channels['Ch2-DeOxy'],color = "red", lw=4,label="DeoxyHb")
+plt.axhline(y=0,xmin=0,xmax=timestamps.shape[0],color="black", linestyle = '--',alpha=0.4)
+plt.title("Channel 2")
+plt.legend(loc="best")
+plt.xlabel("Time [s]"); plt.ylabel(r'$\Delta c$(OxyHb, DeoxyHb)/$\mu$M')
+
+plt.figure( figsize=(6,6),facecolor='white')
+plt.plot(timestamps,channels['Ch3-Oxy'], color="blue", lw=4,label="OxyHb")
+plt.plot(timestamps,channels['Ch3-DeOxy'],color = "red", lw=4,label="DeoxyHb")
+plt.axhline(y=0,xmin=0,xmax=timestamps.shape[0],color="black", linestyle = '--',alpha=0.4)
+plt.title("Channel 3")
+plt.legend(loc="best")
+plt.xlabel("Time [s]"); plt.ylabel(r'$\Delta c$(OxyHb, DeoxyHb)/$\mu$M')
+
+plt.figure( figsize=(6,6),facecolor='white')
+plt.plot(timestamps,channels['Ch4-Oxy'], color="blue", lw=4,label="OxyHb")
+plt.plot(timestamps,channels['Ch4-DeOxy'],color = "red", lw=4,label="DeoxyHb")
+plt.axhline(y=0,xmin=0,xmax=timestamps.shape[0],color="black", linestyle = '--',alpha=0.4)
+plt.title("Channel 4")
+plt.legend(loc="best")
+plt.xlabel("Time [s]"); plt.ylabel(r'$\Delta c$(OxyHb, DeoxyHb)/$\mu$M')
+
+plt.figure( figsize=(6,6),facecolor='white')
+plt.plot(timestamps,channels['Ch5-Oxy'], color="blue", lw=4,label="OxyHb")
+plt.plot(timestamps,channels['Ch5-DeOxy'],color = "red", lw=4,label="DeoxyHb")
+plt.axhline(y=0,xmin=0,xmax=timestamps.shape[0],color="black", linestyle = '--',alpha=0.4)
+plt.title("Channel 5")
+plt.legend(loc="best")
+plt.xlabel("Time [s]"); plt.ylabel(r'$\Delta c$(OxyHb, DeoxyHb)/$\mu$M')
+
+plt.figure( figsize=(6,6),facecolor='white')
+plt.plot(timestamps,channels['Ch6-Oxy'], color="blue", lw=4,label="OxyHb")
+plt.plot(timestamps,channels['Ch6-DeOxy'],color = "red", lw=4,label="DeoxyHb")
+plt.axhline(y=0,xmin=0,xmax=timestamps.shape[0],color="black", linestyle = '--',alpha=0.4)
+plt.title("Channel 6")
+plt.legend(loc="best")
+plt.xlabel("Time [s]"); plt.ylabel(r'$\Delta c$(OxyHb, DeoxyHb)/$\mu$M')
+
+plt.figure( figsize=(6,6),facecolor='white')
+plt.plot(timestamps,channels[ 'Control1-Oxy'], color="blue", lw=4,label="OxyHb")
+plt.plot(timestamps,channels[ 'Control1-DeOxy'],color = "red", lw=4,label="DeoxyHb")
+plt.axhline(y=0,xmin=0,xmax=timestamps.shape[0],color="black", linestyle = '--',alpha=0.4)
+plt.title(" Control1-Oxy")
+plt.legend(loc="best")
+plt.xlabel("Time [s]"); plt.ylabel(r'$\Delta c$(OxyHb, DeoxyHb)/$\mu$M')
+
+plt.figure( figsize=(6,6),facecolor='white')
+plt.plot(timestamps,channels[ 'Control2-Oxy'], color="blue", lw=4,label="OxyHb")
+plt.plot(timestamps,channels[ 'Control2-DeOxy'],color = "red", lw=4,label="DeoxyHb")
+plt.axhline(y=0,xmin=0,xmax=timestamps.shape[0],color="black", linestyle = '--',alpha=0.4)
+plt.title(" Control1-Oxy")
+plt.legend(loc="best")
+plt.xlabel("Time [s]"); plt.ylabel(r'$\Delta c$(OxyHb, DeoxyHb)/$\mu$M')
+
+plt.show()
+
+'''
+
+# Plot deL GRID ------------------------------------------------------------------------
+gridManifold = np.genfromtxt(path_experiment+"Manifold/gridManifold.csv", delimiter=',')
+gridManifold = pd.DataFrame(data={'Phase':gridManifold[:,0],
+                                'PhaseSin':gridManifold[:,1],
+                                'PhaseCos':gridManifold[:,2],
+                                'Frequency':gridManifold[:,3],
+                                'Time':gridManifold[:,4],
+                                'InfoChanel':gridManifold[:,5]})
+# ------------------------------------------------------------------ phase - FREQUENCT
+fig = plt.figure(figsize = (16, 9))
+ax = plt.axes(projection ="3d")
+# Add x, y gridlines
+ax.grid(b = True, color ='grey',linestyle ='-.', linewidth = 0.3, alpha = 0.2)
+# Creating color map
+my_cmap = plt.get_cmap('hsv')
+# Creating plot
+sctt = ax.scatter3D(gridManifold['PhaseSin'], gridManifold['PhaseCos'], gridManifold['Frequency'],
+                    alpha = 0.2,
+                    cmap = my_cmap,
+                    marker ='^')
+plt.title("PHASE - FREQUENCY")
+ax.set_xlabel('COS(PHASE)', fontweight ='bold')
+ax.set_ylabel('SIN(PHASE)', fontweight ='bold')
+ax.set_zlabel('FREQUENCY', fontweight ='bold')
+fig.colorbar(sctt, ax = ax, shrink = 0.5, aspect = 5)
+
+# ------------------------------------------------------------------ phase - TIME
+fig = plt.figure(figsize = (16, 9))
+ax = plt.axes(projection ="3d")
+# Add x, y gridlines
+ax.grid(b = True, color ='grey',linestyle ='-.', linewidth = 0.3, alpha = 0.2)
+# Creating color map
+my_cmap = plt.get_cmap('hsv')
+# Creating plot
+sctt = ax.scatter3D(gridManifold['PhaseSin'], gridManifold['PhaseCos'], gridManifold['Time'],
+                    alpha = 0.2,
+                    cmap = my_cmap,
+                    marker ='^')
+plt.title("PHASE - TIME")
+ax.set_xlabel('COS(PHASE)', fontweight ='bold')
+ax.set_ylabel('SIN(PHASE)', fontweight ='bold')
+ax.set_zlabel('TIME', fontweight ='bold')
+fig.colorbar(sctt, ax = ax, shrink = 0.5, aspect = 5)
+
+# ------------------------------------------------------------------ phase - freq - time
+fig = plt.figure(figsize = (16, 9))
+ax = plt.axes(projection ="3d")
+# Add x, y gridlines
+ax.grid(b = True, color ='grey',linestyle ='-.', linewidth = 0.3, alpha = 0.2)
+# Creating color map
+my_cmap = plt.get_cmap('hsv')
+# Creating plot
+sctt = ax.scatter3D(gridManifold['PhaseCos'], gridManifold['Frequency'], gridManifold['Time'],
+                    alpha = 0.2,
+                    cmap = my_cmap,
+                    marker ='^')
+plt.title("PHASE - TIME")
+ax.set_xlabel('COS(PHASE)', fontweight ='bold')
+ax.set_ylabel('FREQ', fontweight ='bold')
+ax.set_zlabel('TIME', fontweight ='bold')
+fig.colorbar(sctt, ax = ax, shrink = 0.5, aspect = 5)
+plt.show()
+
+# -----------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
+# Plot deL EVENTOS ------------------------------------------------------------------------
+eventsManifold = pd.read_csv(path_experiment+"Manifold/eventsManifold.csv")
+eventsManifold.columns = ["Phase", "PhaseSin", "PhaseCos", "Frequency","Time","InfoChanel","InfoMeasured","PathCones"]
+# ------------------------------------------------------------------ phase - FREQUENCT
+fig = plt.figure(figsize = (16, 9))
+ax = plt.axes(projection ="3d")
+fig3 = px.scatter_3d(gridManifold, x='PhaseSin', y='PhaseCos', z='Frequency',color='Frequency', opacity = 0.1,title="phase - time")
+fig = px.line_3d(eventsManifold, x='PhaseSin', y='PhaseCos', z='Frequency',color='InfoChanel',title="phase - frequency")
+fig2 = px.scatter_3d(eventsManifold, x='PhaseSin', y='PhaseCos', z='Frequency',color='InfoChanel',title="phase - frequency")
+fig.add_trace(fig2.data[0])
+fig.add_trace(fig3.data[0])
+fig.update_coloraxes(showscale=False)
+fig.show()
+
+# ------------------------------------------------------------------ phase - TIME
+fig = plt.figure(figsize = (16, 9))
+ax = plt.axes(projection ="3d")
+fig3 = px.scatter_3d(gridManifold, x='PhaseSin', y='PhaseCos', z='Time',color='Time', opacity = 0.1,title="phase - time")
+fig = px.line_3d(eventsManifold, x='PhaseSin', y='PhaseCos', z='Time',color='InfoChanel',title="phase - time")
+fig2 = px.scatter_3d(eventsManifold, x='PhaseSin', y='PhaseCos', z='Time',color='InfoChanel',title="phase - time")
+fig.add_trace(fig2.data[0])
+fig.add_trace(fig3.data[0])
+fig.update_coloraxes(showscale=False)
+fig.show()
+
+# ------------------------------------------------------------------ phase - freq - time
+fig = plt.figure(figsize = (16, 9))
+ax = plt.axes(projection ="3d")
+fig3 = px.scatter_3d(gridManifold, x='PhaseSin', y='Frequency', z='Time',color='Time', opacity = 0.1,title="phase - frequency- time")
+fig = px.line_3d(eventsManifold, x='PhaseSin', y='Frequency', z='Time',color='InfoChanel',title="phase - frequency- time")
+fig2 = px.scatter_3d(eventsManifold, x='PhaseSin', y='Frequency', z='Time',color='InfoChanel',title="phase - frequency- time")
+fig.add_trace(fig2.data[0])
+fig.add_trace(fig3.data[0])
+fig.update_coloraxes(showscale=False)
+fig.show()
+
+# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+
+for index, row in eventsManifold.iterrows():
+    name = row['PathCones']
+    print(row)
+
+    if os.stat(path_experiment+"Cones/"+str(name)+"/futureCone.csv").st_size != 0:
+        cone = pd.read_csv(path_experiment+"Cones/"+str(name)+"/futureCone.csv")
+        cone.columns = ["Phase", "PhaseSin", "PhaseCos", "Frequency","Time","InfoChanel","InfoMeasured"]
+        fig = px.scatter_3d(cone, x='Phase', y='Frequency', z='Time',title="future")
+        fig2 = px.scatter_3d(gridManifold, x='Phase', y='Frequency', z='Time',color ='Time',opacity = 0.1)
+        fig.add_trace(fig2.data[0])
+        fig.show()
+
+    
+
+    if os.stat(path_experiment+"Cones/"+str(name)+"/pastCone.csv").st_size != 0:
+        cone = pd.read_csv(path_experiment+"Cones/"+str(name)+"/pastCone.csv")
+        cone.columns = ["Phase", "PhaseSin", "PhaseCos", "Frequency","Time","InfoChanel","InfoMeasured"]
+        fig =  px.scatter_3d(cone, x='Phase', y='Frequency', z='Time',title="Past cone")
+        fig2 = px.scatter_3d(gridManifold, x='Phase', y='Frequency', z='Time',color ='Time',opacity = 0.1)
+        fig.add_trace(fig2.data[0])
+        fig.show()
+
+    if os.stat(path_experiment+"Cones/"+str(name)+"/horismosCone.csv").st_size != 0:
+        cone = pd.read_csv(path_experiment+"Cones/"+str(name)+"/horismosCone.csv")
+        cone.columns = ["Phase", "PhaseSin", "PhaseCos", "Frequency","Time","InfoChanel","InfoMeasured"]
+        fig =  px.scatter_3d(cone,x='Phase', y='Frequency', z='Time', title="horismos cone")
+        fig2 = px.scatter_3d(gridManifold, x='Phase', y='Frequency', z='Time',color ='Time',opacity = 0.1)
+        fig.add_trace(fig2.data[0])
+        fig.show()
+
+'''
---
+++ python_scripts/manifold_Experiment2.py
@@ -0,0 +1,173 @@
+import plotly.graph_objects as go
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+from pyrsistent import v
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+path_experiment ="/Users/alerocha/Documents/Causal-Manifold/Experiments/Experiment1/Outputs/"
+
+
+def cylinder(r, h, a =0, nt=100, nv =50):
+    """
+    parametrize the cylinder of radius r, height h, base point a
+    """
+    theta = np.linspace(0, 2*np.pi, nt)
+    v = np.linspace(a, a+h, nv )
+    theta, v = np.meshgrid(theta, v)
+    x = r*np.cos(theta)
+    y = r*np.sin(theta)
+    z = v
+    return x, y, z
+
+def boundary_circle(r, h, nt=100):
+    """
+    r - boundary circle radius
+    h - height above xOy-plane where the circle is included
+    returns the circle parameterization
+    """
+    theta = np.linspace(0, 2*np.pi, nt)
+    x= r*np.cos(theta)
+    y = r*np.sin(theta)
+    z = h*np.ones(theta.shape)
+    return x, y, z
+
+
+def draw_curve(phase1,phase2,z0,z1):
+    """
+    Draw a spiral curve between 2 points
+    """
+    if phase1 == -3.1416:
+        phase1 = 3.1416
+    if phase2 == -3.1416:
+        phase2 = 3.1416
+    if abs(phase1 - phase2)>=np.pi:
+        dir = 1
+    else:
+        dir = 1
+    theta  = np.linspace(phase1, phase2, 100, endpoint=True)
+    thetaz = np.linspace(z0, z1, 100, endpoint=True)
+    helix_x = dir*np.cos(1*theta)
+    helix_y = dir*np.sin(1*theta)
+    helix_z = thetaz
+    return helix_x, helix_y, helix_z
+
+
+# Paso 1: Cargar las observaciones
+eventsManifold = pd.read_csv(path_experiment+"Manifold/eventsManifold.csv")
+eventsManifold.columns = ["Phase", "PhaseSin", "PhaseCos", "Frequency","Time","InfoChanel","InfoMeasured","PathCones"]
+unique_Channels = np.unique(eventsManifold['InfoChanel'])
+namesTraces = []
+curves_channels_phase_time = []
+curves_channels_phase_freq = []
+
+for channel in unique_Channels:
+    #subsampling
+    temp_phase = []
+    temp_freq = []
+    temp_time = []
+    namesTraces.append("Channel: "+str(channel))
+    print(" [->] Computing curve for channel :", channel)
+    current_channel = eventsManifold[eventsManifold["InfoChanel"] == channel]
+    current_channel = current_channel.reset_index()
+    for index, row in current_channel.iterrows():
+        temp_phase.append(row['Phase'])
+        temp_freq.append(row['Frequency'])
+        temp_time.append(row['Time'])
+
+    #Compute curves
+    temp_curve_phase_time_x = []
+    temp_curve_phase_time_y = []
+    temp_curve_phase_time_z = []
+    temp_curve_phase_freq_x = []
+    temp_curve_phase_freq_y = []
+    temp_curve_phase_freq_z = []
+    for i in range(0,len(temp_time)-1,1):
+        ctx,cty, ctz = draw_curve (temp_phase[i],temp_phase[i+1],temp_time[i],temp_time[i+1])
+        cfx,cfy,cfz = draw_curve (temp_phase[i],temp_phase[i+1],temp_freq[i],temp_freq[i+1])
+        temp_curve_phase_time_x.append(ctx)
+        temp_curve_phase_time_y.append(cty)
+        temp_curve_phase_time_z.append(ctz)
+        temp_curve_phase_freq_x.append(cfx)
+        temp_curve_phase_freq_y.append(cfy)
+        temp_curve_phase_freq_z.append(cfz)
+
+    curves_channels_phase_time.append([temp_curve_phase_time_x,temp_curve_phase_time_y,temp_curve_phase_time_z])
+    curves_channels_phase_freq.append([temp_curve_phase_freq_x,temp_curve_phase_freq_y,temp_curve_phase_freq_z])
+
+'''
+print("Curves computed ", len(curves_channels_phase_time))
+for curve in curves_channels_phase_time:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    # Creating plot
+    ax.plot(np.array(curve[0]).flatten(), np.array(curve[1]).flatten(), np.array(curve[2]).flatten()) # the full spiral
+    plt.show()
+'''
+
+r1 = 1
+a1 = 0
+h1 = 60
+x1, y1, z1 = cylinder(r1, h1, a=a1)
+colorscale = [[0, 'blue'],
+             [1, 'blue']]
+cyl1 = go.Surface(x=x1, y=y1, z=z1,
+                 colorscale = colorscale,
+                 showscale=False,
+                 opacity=0.3)
+xb_low, yb_low, zb_low = boundary_circle(r1, h=a1)
+xb_up, yb_up, zb_up = boundary_circle(r1, h=a1+h1)
+bcircles1 =go.Scatter3d(x = xb_low.tolist()+[None]+xb_up.tolist(),
+                        y = yb_low.tolist()+[None]+yb_up.tolist(),
+                        z = zb_low.tolist()+[None]+zb_up.tolist(),
+                        mode ='lines',
+                        line = dict(color='blue', width=2),
+                        opacity =0.55, showlegend=False)
+dataFigs = [cyl1, bcircles1]
+
+for curve,name in zip(curves_channels_phase_time,namesTraces):
+    curveFig =go.Scatter3d(x=np.array(curve[0]).flatten(),
+                        y=np.array(curve[1]).flatten(),
+                        z=np.array(curve[2]).flatten(),
+                        line = dict(width=4),
+                        name = name,
+                        mode='lines') # the full spiral
+    dataFigs.append(curveFig)
+
+
+layout = go.Layout(scene_xaxis_visible=True, scene_yaxis_visible=True, scene_zaxis_visible=True)
+fig = go.Figure(data=dataFigs, layout=layout)
+fig.update_layout(scene_camera_eye_z= 0.55)
+fig.layout.scene.camera.projection.type = "orthographic" #commenting this line you get a fig with perspective proj
+fig.update_layout(scene = dict(
+                    xaxis = dict(
+                        backgroundcolor="rgb(200, 200, 230)",
+                        gridcolor="white",
+                        showbackground=True,
+                        zerolinecolor="white",),
+                    yaxis = dict(
+                        backgroundcolor="rgb(200, 200, 230)",
+                        gridcolor="white",
+                        showbackground=True,
+                        zerolinecolor="white"),
+                    zaxis = dict(
+                        backgroundcolor="rgb(230, 200,230)",
+                        gridcolor="white",
+                        showbackground=True,
+                        zerolinecolor="white",),),
+                    width=700,
+                    margin=dict(
+                    r=10, l=10,
+                    b=10, t=10)
+                )
+fig.update_layout(scene = dict(
+                    xaxis_title=r'Cos(phase)',
+                    yaxis_title=r'Sin(phase)',
+                    zaxis_title=r'Time'),
+                    width=700,
+                    margin=dict(r=20, b=10, l=10, t=10))
+fig.show()
